f628cba5e4d7649ce5791ad212b27c177f6c0d02
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-25 14:14:06 -0300

    Add project files.

---
+++ LaConsolaEjercicio3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaConsolaEjercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            do
+            {
+                Console.WriteLine("Ingrese cualquier tecla, o X para terminar");
+
+            } while (Console.ReadKey().Key != ConsoleKey.X);
+
+        }
+    }
+}
